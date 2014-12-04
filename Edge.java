f83e74f9435aeb5000d097e75c6b12239ad454e9
mckeankt <mckeankt@rams.colostate.edu>
2014-12-04 15:30:41 -0700

    Adds (tested) Graph and Edge classes

---
+++ Edge.java
@@ -0,0 +1,23 @@
+
+//Represents a hyperlink between web pages (Or a directed edge in a graph)
+public class Edge {
+	private String source;
+	private String dest;
+	
+	public Edge(String source, String dest){
+		this.source = source;
+		this.dest = dest;
+	}
+	
+	public String toString(){
+		return "\""+this.source+"\""+" -> "+"\""+this.dest+"\"";
+	}
+
+	public String getSource(){
+		return this.source;
+	}
+	
+	public String getDest(){
+		return this.dest;
+	}
+}
