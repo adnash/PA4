ab52e0ce3ce5dbbaa47002ce4fb060e6789b8409
Alan Nash <adnash@rams.colostate.edu>
2014-12-09 09:45:03 -0700

    spaceing changes

--- Edge.java
+++ Edge.java
@@ -3,20 +3,20 @@
 public class Edge {
 	private String source;
 	private String dest;
-	
+
 	public Edge(String source, String dest){
 		this.source = source;
 		this.dest = dest;
 	}
-	
+
 	public String toString(){
-		return "\""+this.source+"\""+" -> "+"\""+this.dest+"\"";
+		return "\""+this.source+"\""+" -> "+"\""+this.dest+"\";";
 	}
 
 	public String getSource(){
 		return this.source;
 	}
-	
+
 	public String getDest(){
 		return this.dest;
 	}
